f06b3b2a53cdc596a16e779ecf782464edae44f1
Alana Cunha Neri <152909620+NeriAlana@users.noreply.github.com>
2025-08-27 15:38:14 -0300

    Update form.js

--- anexo_desafio_javascript/js/form.js
+++ anexo_desafio_javascript/js/form.js
@@ -3,11 +3,12 @@ const nome = document.getElementById("nomeid");
 //class contato
 
 class contato {
-    constructor(nome, email,telefone,contato){
+    constructor(nome, email,telefone,contato, mensagem){
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.contato = contato;
+        this.mensagem = mensagem;
     }
 }
 
@@ -18,7 +19,8 @@ function Post(evento, form) {
             form.elements.namedItem("nome").value, 
             form.elements.namedItem("email").value,  
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value
+            form.elements.namedItem("contato").value,
+            form.elements.namedItem("mensagem").value
         );
         return data;
 }
@@ -28,8 +30,11 @@ function Enviar() {
     
     form.onsubmit = function (evento) {
         const data = Post(evento, form);
+
+        console.log('Dados do Formulário:', data);
         
-        alert(`Obrigado sr(a) ${nome.value}, os seus dados foram encaminhados com sucesso`);
+
+        alert(`Obrigado sr(a) ${nome.value}, os seus dados foram encaminhados com sucesso.`);
         form.reset();
     };
 }
